abaaec5a12c24697d4b0fb3f7eeda4873c1b72f1
GabrielTakazaki <gabriel_takazaki@hotmail.com>
2018-05-22 20:54:07 -0300

    Mapear filtro no web.xml #5

--- src/main/java/edu/utfpr/filtros/FiltroLinguagens.java
+++ src/main/java/edu/utfpr/filtros/FiltroLinguagens.java
@@ -8,10 +8,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebFilter("/*")
 public class FiltroLinguagens implements Filter{
 
     public void init(FilterConfig filterConfig) throws ServletException {
